c94c52bf24240a0d3443b73caf97fdff7c480a4d
sgoenko <sgoenko@gmail.com>
2021-01-13 19:53:22 +0300

    Test Your New Service with a Unit Test

--- bin/custom/concerttours/testsrc/concerttours/daos/impl/DefaultBandDAOIntegrationTest.java
+++ bin/custom/concerttours/testsrc/concerttours/daos/impl/DefaultBandDAOIntegrationTest.java
@@ -1,10 +1,14 @@
 package concerttours.daos.impl;
+
 import static org.junit.Assert.assertTrue;
+
 import de.hybris.bootstrap.annotations.IntegrationTest;
 import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
 import de.hybris.platform.servicelayer.model.ModelService;
+
 import java.util.List;
 import javax.annotation.Resource;
+
 import org.junit.Assert;
 import org.junit.Test;
 import concerttours.daos.BandDAO;
@@ -12,30 +16,42 @@ import concerttours.model.BandModel;
 
 /**
  * The purpose of this test is to illustrate DAO best practices and behaviour.
- *
+ * <p>
  * The DAO logic is factored into a separate POJO. Stepping into these will illustrate how to write and execute
  * FlexibleSearchQueries - the basis on which most hybris DAOs operate.
  */
 @IntegrationTest
-public class DefaultBandDAOIntegrationTest extends ServicelayerTransactionalTest
-{
-    /** As this is an integration test, the class (object) being tested gets injected here. */
+public class DefaultBandDAOIntegrationTest extends ServicelayerTransactionalTest {
+    /**
+     * As this is an integration test, the class (object) being tested gets injected here.
+     */
+
     @Resource
     private BandDAO bandDAO;
-    /** Platform's ModelService used for creation of test data. */
+    /**
+     * Platform's ModelService used for creation of test data.
+     */
     @Resource
     private ModelService modelService;
-    /** Name of test band. */
+    /**
+     * Name of test band.
+     */
     private static final String BAND_CODE = "ROCK-11";
-    /** Name of test band. */
+    /**
+     * Name of test band.
+     */
     private static final String BAND_NAME = "Ladies of Rock";
-    /** History of test band. */
+    /**
+     * History of test band.
+     */
     private static final String BAND_HISTORY = "All female rock band formed in Munich in the late 1990s";
-    /** Albums sold */
+    /**
+     * Albums sold
+     */
     private static final Long ALBUMS_SOLD = Long.valueOf(1000L);
+
     @Test
-    public void bandDAOTest()
-    {
+    public void bandDAOTest() {
         // check that our test band is not already present in the database
         List<BandModel> bandsByCode = bandDAO.findBandsByCode(BAND_CODE);
         assertTrue("No Band should be returned", bandsByCode.isEmpty());
@@ -61,17 +77,16 @@ public class DefaultBandDAOIntegrationTest extends ServicelayerTransactionalTest
         Assert.assertEquals("Retrieved Band's albumSales attribute incorrect", ALBUMS_SOLD, bandsByCode.get(0).getAlbumSales());
         Assert.assertEquals("Retrieved Band's history attribute incorrect", BAND_HISTORY, bandsByCode.get(0).getHistory());
     }
+
     @Test
-    public void testFindBands_EmptyStringParam()
-    {
+    public void testFindBands_EmptyStringParam() {
         //calling findBandsByCode() with an empty String - returns no results
         final List<BandModel> bands = bandDAO.findBandsByCode("");
         Assert.assertTrue("No Band should be returned", bands.isEmpty());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testfindBands_NullParam()
-    {
+    public void testfindBands_NullParam() {
         //calling findBandByCode with null should throw an IllegalArgumentException
         bandDAO.findBandsByCode(null); //method's return value not captured
     }
--- bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceIntegrationTest.java
+++ bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package concerttours.service.impl;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
 import de.hybris.bootstrap.annotations.IntegrationTest;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.jalo.CoreBasicDataCreator;
@@ -8,11 +10,13 @@ import de.hybris.platform.servicelayer.ServicelayerTest;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
 import de.hybris.platform.servicelayer.model.ModelService;
 import de.hybris.platform.variants.model.VariantProductModel;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import javax.annotation.Resource;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,23 +24,30 @@ import concerttours.model.BandModel;
 import concerttours.service.BandService;
 
 @IntegrationTest
-public class DefaultBandServiceIntegrationTest extends ServicelayerTest
-{
+public class DefaultBandServiceIntegrationTest extends ServicelayerTest {
     @Resource
     private BandService bandService;
     @Resource
     private ModelService modelService;
-    /** Test band */
+    /**
+     * Test band
+     */
     private BandModel bandModel;
-    /** Name of test band. */
+    /**
+     * Name of test band.
+     */
     private static final String BAND_CODE = "101-JAZ";
-    /** Name of test band. */
+    /**
+     * Name of test band.
+     */
     private static final String BAND_NAME = "Tight Notes";
-    /** History of test band. */
+    /**
+     * History of test band.
+     */
     private static final String BAND_HISTORY = "New contemporary, 7-piece Jaz unit from London, formed in 2015";
+
     @Before
-    public void setUp()
-    {
+    public void setUp() {
         // This instance of a BandModel will be used by the tests
         bandModel = modelService.create(BandModel.class);
         bandModel.setCode(BAND_CODE);
@@ -44,18 +55,18 @@ public class DefaultBandServiceIntegrationTest extends ServicelayerTest
         bandModel.setAlbumSales(null);
         bandModel.setHistory(BAND_HISTORY);
     }
+
     @Test(expected = UnknownIdentifierException.class)
-    public void testFailBehavior()
-    {
+    public void testFailBehavior() {
         bandService.getBandForCode(BAND_CODE);
     }
+
     /**
      * This test tests and demonstrates that the Service's getAllBand method calls the DAOs' getAllBand method and
      * returns the data it receives from it.
      */
     @Test
-    public void testBandService()
-    {
+    public void testBandService() {
         List<BandModel> bandModels = bandService.getBands();
         final int size = bandModels.size();
         modelService.save(bandModel);
@@ -66,13 +77,13 @@ public class DefaultBandServiceIntegrationTest extends ServicelayerTest
         assertNotNull("No band found", persistedBandModel);
         assertEquals("Different band found", bandModel, persistedBandModel);
     }
+
     /**
      * This test tests and demonstrates that the Service's getAllBand method calls the DAOs' getAllBand method and
      * returns the data it receives from it.
      */
     @Test
-    public void testBandServiceTours() throws Exception
-    {
+    public void testBandServiceTours() throws Exception {
         createCoreData();
         importCsv("/impex/concerttours-bands.impex", "utf-8");
         importCsv("/impex/concerttours-yBandTour.impex", "utf-8");
